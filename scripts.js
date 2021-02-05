6356e6ffc50494f81d7276400223cc708b4b7517
Zenia Gist <zeniagist@gmail.com>
2021-02-05 01:57:47 +0000

    updated adding cross out functionality to jQuery

--- scripts.js
+++ scripts.js
@@ -1,29 +1,31 @@
 
 function newItem(){
 
-//javascript
+//jQuery
 //1. Adding a new item to the list of items: 
   let li = $("<li></li>");
   let inputValue = $("input").val(); //getting input values
   li.append(inputValue);
 
-if (inputValue === '') {
-  alert("You must write something!");
-} else {
-  $('#list').append(li);
-}
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    $('#list').append(li);
+  }
 
 //2. Crossing out an item from the list of items:
   function crossOut() {
-  li.classList.toggle("strike");
-}
+      li.toggleClass("strike");
+    }
 
-li.addEventListener("dblclick",crossOut);
+    li.on("dblclick", function crossOut() {
+      li.toggleClass("strike");
+    });
 
 //3(i). Adding the delete button "X": 
   let crossOutButton = document.createElement("crossOutButton");
-crossOutButton.appendChild(document.createTextNode("X"));
-li.appendChild(crossOutButton);
+  crossOutButton.appendChild(document.createTextNode("X"));
+  li.appendChild(crossOutButton);
 
 crossOutButton.addEventListener("click", deleteListItem);
 //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
