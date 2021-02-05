7ce13a7a44e0f2679243ec82c6e874cc7a70cc6c
Zenia Gist <zeniagist@gmail.com>
2021-02-05 02:03:17 +0000

    added clear input function after adding to list

--- scripts.js
+++ scripts.js
@@ -13,6 +13,9 @@ function newItem(){
     $('#list').append(li);
   }
 
+  //clear input 
+  $("input").val("");
+
 //2. Crossing out an item from the list of items:
   function crossOut() {
       li.toggleClass("strike");
@@ -35,59 +38,3 @@ function newItem(){
   $('#list').sortable();
 
 }
-
-
-
-
-
-// If you get stuck, you can look below for the jQuery code. However, try yourself to convert the vanilla JS code provided to jQuery first.
-
-
-
-
-
-
-
-
-  /*
-// jQuery Code
-//1. Adding a new item to the list:
-
-  let li = $('<li></li>');
-  let inputValue = $('#input').val();
-  li.append(inputValue);
-
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    $('#list').append(li);
-  }
-//2. Crossing an item out:
-  function crossOut() {
-		li.toggleClass("strike");
-	}
-
-	li.on("dblclick", function crossOut() {
-		li.toggleClass("strike");
-	});
-//3. Adding a delete button
-  let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append(document.createTextNode('X'));
-  li.append(crossOutButton);
-
-//   crossOutButton.on("click", deleteListItem);
-//   function deleteListItem(){
-// 		li.addClass("delete")
-// 	}
-   $('#list').sortable();
-*/
-
-
-
-
-
-
-
-
-
-
